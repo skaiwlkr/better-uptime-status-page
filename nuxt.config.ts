8543c2107231907b88ac50b78332e6ec378ea386
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2023-04-25 21:13:41 +0200

    added runtime config and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,5 +15,24 @@ export default defineNuxtConfig({
         }
       }
     }
+  },
+  runtimeConfig: {
+    public: {
+      betterstackUrl: process.env.BETTERSTACK_URL,
+      betterstackToken: process.env.BETTERSTACK_TOKEN
+    }
+  },
+  // TODO: Add favicons
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.png' },
+        { rel: 'icon', type: 'image/png', href: '/favicons/favicon_32x32.png' },
+        { rel: 'icon', type: 'image/png', href: '/favicons/favicon_96x96.png' },
+        { rel: 'apple-touch-icon-precomposed', href: '/apple-touch-icon.png' },
+      ]
+    }
   }
 })
