778ee5a6bc0ce7f80ac7bc173db8e5b95ec513e3
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2023-04-25 20:57:17 +0200

    added basic styles and added font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  css: ['~/assets/styles/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/foundation/foundation.scss" as *;'
+        }
+      }
+    }
+  }
 })
