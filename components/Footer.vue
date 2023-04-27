622dc88003a535524df34976a1e0d30c2df1afbd
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2023-04-27 20:40:11 +0200

    updated footer, housekeeping

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-      <div class="flex w-full h-16 mt-16 items-center justify-center border-t border-border-light">
-        <NuxtLink class="text-link" to="https://github.com/skaiwlkr/better-uptime-status-page" target="_blank" title="See on GitHub">GitHub</NuxtLink>
+    <div class="flex px-10 mt-16 mx-auto max-w-4xl">
+      <div class="flex w-full pt-4 border-t border-gray-light">
+        <span class="text-dark">Crafted by <NuxtLink class="text-link" to="https://github.com/skaiwlkr">Sebastian Kaier</NuxtLink></span>
+        <NuxtLink class="ml-auto text-link" to="https://github.com/skaiwlkr/better-uptime-status-page" target="_blank" title="See on GitHub">GitHub</NuxtLink>
       </div>
+    </div>
   </div>
 </template>
